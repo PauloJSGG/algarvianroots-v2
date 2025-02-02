6ab8d11d4f8d96e9cffa99545a1bd87c52456d6d
Valadas, Jorge <>
2025-02-02 15:48:39 +0100

    feat: add video component

---
+++ components/Video.tsx
@@ -0,0 +1,48 @@
+type VideoProps = {
+  desktop: {
+    src: string;
+    poster: string;
+  };
+  mobile: {
+    src: string;
+    poster: string;
+  };
+  className?: string;
+};
+
+export const Video = ({ desktop, mobile, className }: VideoProps) => {
+  return (
+    <div className={className}>
+      <picture>
+        <source media="(max-width: 640px)" srcSet={mobile.poster} />
+        <source media="(min-width: 641px)" srcSet={desktop.poster} />
+        <img
+          src={desktop.poster}
+          alt="Video poster"
+          style={{ display: "none" }}
+        />
+      </picture>
+      <video
+        className="object-cover w-full h-full"
+        autoPlay
+        loop
+        muted
+        playsInline
+        poster={desktop.poster}
+      >
+        <source
+          key="mobile-mp4"
+          src={mobile.src}
+          type="video/mp4"
+          media="(max-width: 640px)"
+        />
+        <source
+          key="desktop-mp4"
+          src={desktop.src}
+          type="video/mp4"
+          media="(min-width: 641px)"
+        />
+      </video>
+    </div>
+  );
+};
