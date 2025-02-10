f3a9b623e8d33277efcb671350dfbf77144777e5
PauloJSGG <paulojsgg@gmail.com>
2025-02-10 23:40:14 +0000

    wip

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,54 @@ import type { NextConfig } from "next";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
+  // consfigure hostname firebasestorage.googleapis.com
   images: {
     domains: ["firebasestorage.googleapis.com", "i.natgeofe.com"],
   },
-
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        "@": require.resolve("."),
-      },
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
+  webpack(config) {
+    // add SVG as asset
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      type: "asset/resource",
+      resourceQuery: /url/, // *.svg?url
+    });
+    // handle svgs as icons
+    config.module.rules.push({
+      test: /public\/.*\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: [/url/] },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            typescript: true,
+            replaceAttrValues: { "#000": "currentColor" },
+            svgProps: {
+              "aria-hidden": "true",
+              role: "presentation",
+              focusable: "false",
+            },
+            svgo: true,
+            svgoConfig: {
+              floatPrecision: 2,
+              plugins: [
+                {
+                  name: "convertStyleToAttrs",
+                },
+                {
+                  name: "convertPathData",
+                  params: {
+                    floatPrecision: 2,
+                  },
+                },
+              ],
+            },
+          },
         },
-      },
-    },
+      ],
+    });
+    return config;
   },
 };
 
